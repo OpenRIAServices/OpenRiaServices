14a0e8e81ab38dfee4d01d050897f80aeb31ce1f
Colin Blair <mail@colinblair.com>
2014-04-07 18:30:13 -0500

    Cloning in DomainController from Microsoft

---
+++ OpenRiaServices.DomainControllers.Server/Framework/DataControllerActionInvoker.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace OpenRiaServices.DomainControllers.Server
+{
+    internal sealed class DataControllerActionInvoker : ApiControllerActionInvoker
+    {
+        public override Task<HttpResponseMessage> InvokeActionAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            DataController controller = (DataController)actionContext.ControllerContext.Controller;
+            controller.ActionContext = actionContext;
+            return base.InvokeActionAsync(actionContext, cancellationToken);
+        }
+    }
+}
---
+++ OpenRiaServices.DomainControllers.Server/Framework/DeleteAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+
+namespace OpenRiaServices.DomainControllers.Server
+{
+    /// <summary>
+    /// Attribute applied to a <see cref="DataController"/> method to indicate that it is a delete method.
+    /// </summary>
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property,
+        AllowMultiple = false, Inherited = true)]
+    public sealed class DeleteAttribute : Attribute
+    {
+    }
+}
