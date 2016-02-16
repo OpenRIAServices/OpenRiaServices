c6b05d0461dc1db71cc1f4c346cde62781641c09
danneesset <daniel.svensson@hotmail.se>
2016-02-16 21:38:41 +0100

    Update Desktop to .Net 4.5 for INotifyDataErrorInfo
    Add INotifyDataErrorInfo implementations for ComplexObjects and Entity
    on for Desktop API

--- OpenRiaServices.DomainServices.Client/Framework/Silverlight/Data/ComplexObject.INotifyDataErrorInfo.cs
+++ OpenRiaServices.DomainServices.Client/Framework/Silverlight/Data/ComplexObject.INotifyDataErrorInfo.cs
@@ -3,39 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace OpenRiaServices.DomainServices.Client
 {
-    public abstract partial class Entity : INotifyDataErrorInfo
+    public abstract partial class ComplexObject : INotifyDataErrorInfo
     {
-        private EventHandler<DataErrorsChangedEventArgs> _validationErrorsChanged;
-
+        private EventHandler<DataErrorsChangedEventArgs> _errorsChangedHandler;
         /// <summary>
         /// Raises the event whenever validation errors have changed for a property.
         /// </summary>
         /// <param name="propertyName">The property whose errors have changed.</param>
         private void RaiseValidationErrorsChanged(string propertyName)
         {
-            EventHandler<DataErrorsChangedEventArgs> handler = this._validationErrorsChanged;
-
-            if (handler != null)
+            if (this._errorsChangedHandler != null)
             {
-                handler(this, new DataErrorsChangedEventArgs(propertyName));
+                this._errorsChangedHandler(this, new DataErrorsChangedEventArgs(propertyName));
             }
         }
-
         /// <summary>
         /// Explicitly implement the <see cref="INotifyDataErrorInfo.ErrorsChanged"/> event.
         /// </summary>
         event EventHandler<DataErrorsChangedEventArgs> INotifyDataErrorInfo.ErrorsChanged
         {
-            add { this._validationErrorsChanged += value; }
-            remove { this._validationErrorsChanged -= value; }
+            add { this._errorsChangedHandler += value; }
+            remove { this._errorsChangedHandler -= value; }
         }
-
         /// <summary>
-        /// Get the errors for the specified property, or the entity-level
+        /// Get the errors for the specified property, or the type-level
         /// errors if <paramref name="propertyName"/> is <c>null</c> of empty.
         /// </summary>
         /// <param name="propertyName">
@@ -44,36 +42,33 @@ namespace OpenRiaServices.DomainServices.Client
         /// </param>
         /// <returns>
         /// The list of errors for the specified <paramref name="propertyName"/>,
-        /// or entity-level errors when <paramref name="propertyName"/> is
+        /// or type-level errors when <paramref name="propertyName"/> is
         /// <c>null</c> or empty.
         /// </returns>
         IEnumerable INotifyDataErrorInfo.GetErrors(string propertyName)
         {
             IEnumerable<ValidationResult> results;
-
             if (string.IsNullOrEmpty(propertyName))
             {
                 // If the property name is null or empty, then we want to include errors
                 // where the member names array is empty, or where the member names array
                 // contains a null or empty string.
-                results = this.ValidationErrors.Where(e => !e.MemberNames.Any() || e.MemberNames.Contains(propertyName));
+                results = this.ValidationResultCollection.Where(e => !e.MemberNames.Any() || e.MemberNames.Contains(propertyName));
             }
             else
             {
                 // Otherwise, only return the errors that contain the property name
-                results = this.ValidationErrors.Where(e => e.MemberNames.Contains(propertyName));
+                results = this.ValidationResultCollection.Where(e => e.MemberNames.Contains(propertyName));
             }
-
             // Prevent deferred enumeration
             return results.ToArray();
         }
-
         /// <summary>
         /// Gets a value indicating whether or not the entity presently has errors.
         /// </summary>
         bool INotifyDataErrorInfo.HasErrors
         {
-            get { return this.HasValidationErrors; }
+            get { return this.ValidationResultCollection.Count > 0; }
         }
     }
 }
