bd5ca73cfa740098d9a3957b16387889504a8656
danneesset <daniel.svensson@hotmail.se>
2016-03-28 17:26:00 +0200

    Fix RiaServicesContrib.EntityFramework compilation

--- RiaServicesContrib.DomainServices.EntityFramework/Framework/DbContextExtensions.cs
+++ RiaServicesContrib.DomainServices.EntityFramework/Framework/DbContextExtensions.cs
@@ -53,7 +53,7 @@ namespace System.ServiceModel.DomainServices.EntityFramework
             }
 
             ObjectContext objectContext = (dbContext as IObjectContextAdapter).ObjectContext;
-            ObjectStateEntry stateEntry = ObjectContextUtilities.AttachAsModifiedInternal<T>(current, original, objectContext);            
+            ObjectStateEntry stateEntry = ObjectContextUtilities.AttachAsModifiedInternal(current, original, objectContext);            
 
             if (stateEntry.State != EntityState.Modified)
             {
