5f84ba318b794db948319a5860e9a277bd90dd98
danneesset <daniel.svensson@hotmail.se>
2016-03-28 10:43:04 +0200

    Add AssemblyInfo.cs to ComboBoxExtensions to Fix #85

---
+++ OpenRiaServices.Silverlight.ComboBoxExtensions/Framework/Silverlight/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenRiaServices.Silverlight.ComboBoxExtensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("Open RIA Services")]
+[assembly: AssemblyCopyright("© Outercurve Foundation.  All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.5.0")]
