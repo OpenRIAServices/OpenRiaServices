319aafb8c4b1670cdea1fb68d68d5aa6c7cd40a6
danneesset <daniel.svensson@hotmail.se>
2016-04-17 13:39:33 +0200

    Add appveyor build file

--- RiaServicesContrib.DomainServices.EntityFramework/Framework/Properties/AssemblyInfo.cs
+++ RiaServicesContrib.DomainServices.EntityFramework/Framework/Properties/AssemblyInfo.cs
@@ -17,8 +17,6 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: InternalsVisibleTo("OpenRiaServices.Common.DomainServices.Test")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
