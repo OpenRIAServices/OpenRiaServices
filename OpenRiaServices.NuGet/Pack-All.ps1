9e0a822016c7b5a2536d81e22f3516b41dcef2af
danneesset <daniel.svensson@hotmail.se>
2016-08-16 22:19:55 +0200

    Add script for creating nuget packages

---
+++ OpenRiaServices.NuGet/Pack-All.ps1
@@ -0,0 +1,34 @@
+param(
+	[string[]]$Path = "OpenRiaServices.*",
+	[string]$Version = $null,
+	[string]$NuGetPath
+)
+
+$scriptPath = (Split-Path -Parent $PSCommandPath)
+Push-Location $scriptPath
+
+$outputDir = "bin"
+if (-not (Test-Path $outputDir)) {
+    mkdir $outputDir
+}
+
+# If NuGet path is not specified then chekc one folder above git repo, or hope for it to be in the path
+if ([string]::IsNullOrEmpty($NuGetPath))
+{
+	if (Test-Path ..\..\NuGet.exe) { $NuGetPath = "..\..\NuGet.exe"}
+	else { $NuGetPath = "nuget.exe"}
+}
+[string[]]$NuGetParameters = @("-OutputDirectory", "$outputDir")
+if (-not [string]::IsNullOrEmpty($Version)) {$NuGetParameters = $NuGetParameters + @("-Version", $Version)}
+
+foreach($folder in (dir $Path | where {[System.IO.Directory]::Exists($_)}))
+{
+    $targets = (dir "$folder\*.nuspec")
+    foreach($nuspec in $targets)
+    {
+        echo "Building $nuspec"
+        & $NuGetPath pack ($nuspec) $NuGetParameters
+    }
+}
+
+Pop-Location
