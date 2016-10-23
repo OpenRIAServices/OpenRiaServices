b6c9372c58292566cbc9d5b6044bc48895f4d126
danneesset <daniel.svensson@hotmail.se>
2016-10-23 22:02:03 +0200

    Update the last server projects to .net 4.5

--- OpenRiaServices.DomainServices.EntityFramework/Framework/MetadataResource.Designer.cs
+++ OpenRiaServices.DomainServices.EntityFramework/Framework/MetadataResource.Designer.cs
@@ -39,7 +39,7 @@ namespace System.Data.Mapping {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenRiaServices.VisualStudio.DomainServices.Tools.MetadataResource", typeof(MetadataResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenRiaServices.DomainServices.EntityFramework.MetadataResource", typeof(MetadataResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
