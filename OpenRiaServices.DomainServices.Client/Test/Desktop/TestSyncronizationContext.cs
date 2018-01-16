3d971459a5fe6cab8c4c2f6d35265b5ae46561a9
Daniel-Svensson <daniel.svensson@hotmail.se>
2018-01-16 20:21:21 +0100

    Resolve race condition in AuthenticationService tests
    * Execute cancellation immediately in tests to resolve race condition in tests

---
+++ OpenRiaServices.DomainServices.Client/Test/Desktop/TestSyncronizationContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace OpenRiaServices.DomainServices.Client.Test
+{
+    /// <summary>
+    /// A <see cref="SynchronizationContext"/> which executes all actions immediately.
+    /// </summary>
+    class TestSynchronizationContext : SynchronizationContext
+    {
+        /// <summary>
+        /// Turn async execution into sync execution
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="state"></param>
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            d(state);
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            d(state);
+        }
+
+        public override SynchronizationContext CreateCopy()
+        {
+            return new TestSynchronizationContext();
+        }
+    }
+}
