OpenRIAServices/OpenRiaServices

commit 769ce4c012648951fda8fa3e8470d1229009888e

Make MetaType and MetaMember public in "Internal" namespace (#129)
* Move MetaType/MetaMember to separate files in Internal namespace
* Add property comments and cleanup some reflection startup logic

OpenRiaServices.DomainServices.Client/Framework/Silverlight/Data/Internal/MetaMember.cs
@@ -1,0 +1,252 @@
+﻿using OpenRiaServices.DomainServices.Server.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace OpenRiaServices.DomainServices.Client.Internal
+{
+    /// <summary>
+    /// INTERNAL - Public API surface might change from version to version.
+    /// This class caches all the interesting attributes of an property.
+    /// </summary>
+    [DebuggerDisplay("Name = {Member.Name}")]
+    public sealed class MetaMember
+    {
+#if !NETSTANDARD1_3
+        private static MethodInfo s_getterDelegateHelper = typeof(MetaMember).GetMethod(nameof(MetaMember.CreateGetterDelegateHelper), BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+        private static MethodInfo s_setterDelegateHelper = typeof(MetaMember).GetMethod(nameof(MetaMember.CreateSetterDelegateHelper), BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+
+        private Func<object, object> _getter;
+        private Action<object, object> _setter;
+#endif
+
+        internal MetaMember(MetaType metaType, PropertyInfo property, bool isRoundtripEntity)
+        {
+            this.Member = property;
+            this.MetaType = metaType;
+
+            bool hasGetter = (property.GetGetMethod() != null);
+
+            IsCollection = TypeUtility.IsSupportedCollectionType(property.PropertyType);
+            if (hasGetter && (property.GetSetMethod() != null))
+            {
+                IsComplex = TypeUtility.IsSupportedComplexType(property.PropertyType);
+                IsDataMember = IsComplex || TypeUtility.IsPredefinedType(property.PropertyType);
+            }
+
+            if (hasGetter)
+            {
+                RequiresValidation = hasGetter && TypeUtility.IsAttributeDefined(property, typeof(ValidationAttribute), true);
+            }
+
+            IsExternalReference = TypeUtility.IsAttributeDefined(property, typeof(ExternalReferenceAttribute), true);
+            IsKeyMember = TypeUtility.IsAttributeDefined(property, typeof(KeyAttribute), false);
+            IsComposition = TypeUtility.IsAttributeDefined(property, typeof(CompositionAttribute), false);
+
+            AssociationAttribute = (AssociationAttribute)property.GetCustomAttributes(typeof(AssociationAttribute), false).SingleOrDefault();
+            EditableAttribute = (EditableAttribute)property.GetCustomAttributes(typeof(EditableAttribute), false).SingleOrDefault();
+
+            IsRoundtripMember = CheckIfRoundtripMember(this, isRoundtripEntity);
+            IsMergable = CheckIfMergeableMember(this);
+        }
+
+        /// <summary>
+        /// Gets the name of the property
+        /// </summary>
+        public string Name => Member.Name;
+
+        /// <summary>
+        /// Gets the CLR type of this property
+        /// </summary>
+        public Type PropertyType => Member.PropertyType;
+
+        /// <summary>
+        /// Get the <see cref="MetaType"/> where the property is defined
+        /// </summary>
+        public MetaType MetaType { get; }
+
+        /// <summary>
+        /// Gets any <see cref="EditableAttribute"/> applied to the property, or <c>null</c>
+        /// if no attribute is specified for the property
+        /// </summary>
+        public EditableAttribute EditableAttribute { get; }
+
+        /// <summary>
+        /// Gets the underlying <see cref="PropertyInfo"/> for the property which this 
+        /// instance represents.
+        /// </summary>
+        internal PropertyInfo Member { get; }
+
+        /// <summary>
+        /// Returns <c>true</c> if the property has a <see cref="AssociationAttribute"/> applied
+        /// and is therefore part of a data relationship (such as a foreign key)
+        /// </summary>
+        public bool IsAssociationMember { get { return AssociationAttribute != null; } }
+
+        /// <summary>
+        /// Gets any <see cref="AssociationAttribute"/> applied to the property, or <c>null</c>
+        /// if no attribute is specified for the property
+        /// </summary>
+        public AssociationAttribute AssociationAttribute { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this member is one of the supported values to send between 
+        /// server and client (and it has both getter and setter).
+        /// It is used to determine if it is part of the data contract between server and client. 
+        /// </summary>
+        public bool IsDataMember { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this member is part of the <see cref="MetaType"/>s key.
+        /// </summary>
+        public bool IsKeyMember { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this member is part of the <see cref="MetaType"/>s key.
+        /// </summary>
+        public bool IsRoundtripMember { get; }
+
+        /// <summary>
+        /// Determines whether the specified type is a complex type or a collection of
+        /// complex types.
+        /// </summary>
+        public bool IsComplex { get; }
+
+        /// <summary>
+        /// <c>true</c> if the member is annotated with a <see cref="ExternalReferenceAttribute"/>
+        /// </summary>
+        public bool IsExternalReference { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this member is a supported collection type.
+        /// </summary>
+        public bool IsCollection { get; }
+
+        /// <summary>
+        /// Returns <c>true</c> if the member has a property validator.
+        /// </summary>
+        /// <remarks>The return value does not take into account whether or not the member requires
+        /// type validation.</remarks>
+        public bool RequiresValidation { get; }
+
+        /// <summary>
+        /// Get the value of the member
+        /// </summary>
+        /// <param name="instance">the instance from which the member should be accessed</param>
+        /// <returns>the value of the property</returns>
+        public object GetValue(object instance)
+        {
+#if NETSTANDARD1_3
+            return Member.GetValue(instance);
+#else
+            if (_getter == null)
+            {
+                _getter = CreateGetterDelegate(Member);
+            }
+            return _getter(instance);
+#endif
+        }
+
+        /// <summary>
+        /// Set the value of the member
+        /// </summary>
+        /// <param name="instance">the instance from which the member should be accessed</param>
+        /// <param name="value">the value to set</param>
+        /// <returns>the value of the property</returns>
+        public void SetValue(object instance, object value)
+        {
+#if NETSTANDARD1_3
+            Member.SetValue(instance, value);
+#else
+            if (_setter == null)
+            {
+                _setter = CreateSetterDelegate(Member);
+            }
+            _setter(instance, value);
+#endif
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this member is mergable 
+        /// (should be updated when loading with behaviour <see cref="LoadBehavior.MergeIntoCurrent"/> or <see cref="LoadBehavior.RefreshCurrent"/>).
+        /// Supported types are mergable by default unless a <see cref="MergeAttribute"/> is defined with <see cref="MergeAttribute.IsMergeable"/> set to false.
+        /// </summary>
+        public bool IsMergable { get; }
+
+        /// <summary>
+        /// <c>true</c> if the member is marked with a <see cref="CompositionAttribute"/>
+        /// </summary>
+        public bool IsComposition { get; }
+
+#if !NETSTANDARD1_3
+        /// <summary>
+        /// Helper method which creates a delegate which can be used to invoke a specific getter
+        /// </summary>
+        /// <param name="propertyInfo"></param>
+        /// <returns></returns>
+        private static Func<object, object> CreateGetterDelegate(PropertyInfo propertyInfo)
+        {
+            var helper = s_getterDelegateHelper.MakeGenericMethod(propertyInfo.DeclaringType, propertyInfo.PropertyType);
+            return (Func<object, object>)helper.Invoke(null, new[] { propertyInfo });
+        }
+
+        private static Func<object, object> CreateGetterDelegateHelper<T, Tprop>(PropertyInfo propertyInfo)
+        {
+            var getMethod = propertyInfo.GetGetMethod();
+            if (getMethod == null)
+            {
+                // If no getter was found, fallback to method throw same type of exception
+                // which exception would do, these should never propagate to the user
+                return obj => { throw new ArgumentException("Internal error: No getter"); };
+            }
+
+            var getter = (Func<T, Tprop>)Delegate.CreateDelegate(typeof(Func<T, Tprop>), getMethod);
+            // Add a wrapper which performs boxing of the function
+            return (object instance) => (object)getter((T)instance);
+        }
+
+        /// <summary>
+        /// Helper method which creates a delegate which can be used to invoke a specific getter
+        /// </summary>
+        /// <param name="propertyInfo"></param>
+        /// <returns></returns>
+        private static Action<object, object> CreateSetterDelegate(PropertyInfo propertyInfo)
+        {
+            var helper = s_setterDelegateHelper.MakeGenericMethod(propertyInfo.DeclaringType, propertyInfo.PropertyType);
+            return (Action<object, object>)helper.Invoke(null, new[] { propertyInfo });
+        }
+
+        private static Action<object, object> CreateSetterDelegateHelper<T, Tprop>(PropertyInfo propertyInfo)
+        {
+            // If no getter was found, fallback to using reflection which will throw exception
+            var setMethod = propertyInfo.GetSetMethod();
+            if (setMethod == null)
+            {
+                // If no setter was found, fallback to method throw same type of exception
+                // which exception would do, these should never propagate to the user
+                return (obj, val) => { throw new ArgumentException("Internal error: No setter"); };
+            }
+
+            var setter = (Action<T, Tprop>)Delegate.CreateDelegate(typeof(Action<T, Tprop>), setMethod);
+            // Add a wrapper which performs unboxing for the function
+            return (object obj, object value) => setter((T)obj, (Tprop)value);
+        }
+#endif
+
+        private static bool CheckIfMergeableMember(MetaMember metaMember)
+        {
+            return metaMember.IsDataMember && !metaMember.IsAssociationMember &&
+                   (TypeUtility.IsAttributeDefined(metaMember.Member, typeof(MergeAttribute), true) == false ||
+                   ((MergeAttribute)metaMember.Member.GetCustomAttributes(typeof(MergeAttribute), true).First()).IsMergeable);
+        }
+
+
+        private static bool CheckIfRoundtripMember(MetaMember metaMember, bool isRoundtripEntity)
+        {
+            return metaMember.IsDataMember && !metaMember.IsAssociationMember &&
+                       (isRoundtripEntity || TypeUtility.IsAttributeDefined(metaMember.Member, typeof(RoundtripOriginalAttribute), false));
+        }
+    }
+}

OpenRiaServices.DomainServices.Client/Framework/Silverlight/Data/Internal/MetaType.cs
@@ -1,0 +1,343 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using OpenRiaServices.DomainServices.Server.Data;
+
+namespace OpenRiaServices.DomainServices.Client.Internal
+{
+    /// <summary>
+    /// INTERNAL - Public API surface might change from version to version.
+    /// 
+    /// Class providing additional "meta" information for a Type.
+    /// <remarks>
+    /// Consider adding any commonly accessed or computed information about a Type
+    /// to this class, to improve performance and code factoring.
+    /// MetaType must be fully readonly before GetMetaType returns. While it is cached per thread,
+    /// it may be cached on types elsewhere providing multithreaded access to visible properties.
+    /// </remarks>
+    /// </summary>
+    [DebuggerDisplay("Type = {_type.Name}")]
+    public sealed class MetaType
+    {
+        /// <summary>
+        /// We're using TLS for performance to avoid taking locks. In multithreaded
+        /// scenarios this means that there may be multiple MetaType caches around
+        /// but that shouldn't be a problem.
+        /// </summary>
+        [ThreadStatic]
+        private static Dictionary<Type, MetaType> s_metaTypes;
+        private readonly bool _requiresValidation;
+        private readonly Type[] _childTypes;
+        private readonly Dictionary<string, MetaMember> _metaMembers = new Dictionary<string, MetaMember>();
+        private readonly ReadOnlyCollection<MetaMember> _dataMembers;
+        private readonly ReadOnlyCollection<ValidationAttribute> _validationAttributes;
+        private readonly Dictionary<string, EntityActionAttribute> _customUpdateMethods;
+
+        /// <summary>
+        /// Returns the MetaType for the specified Type.
+        /// </summary>
+        /// <param name="type">The Type to provide the MetaType for.</param>
+        /// <returns>The constructed MetaType.</returns>
+        public static MetaType GetMetaType(Type type)
+        {
+            Debug.Assert(!TypeUtility.IsPredefinedType(type), "Should never attempt to create a MetaType for a base type.");
+
+            MetaType metaType = null;
+            if (!MetaTypes.TryGetValue(type, out metaType))
+            {
+                metaType = new MetaType(type);
+                MetaTypes[type] = metaType;
+            }
+            return metaType;
+        }
+
+        private MetaType(Type type)
+        {
+            _customUpdateMethods = (from method in type.GetMethods(MemberBindingFlags)
+                                    let attributes = method.GetCustomAttributes(typeof(EntityActionAttribute), false)
+                                    let attribute = (EntityActionAttribute)attributes.FirstOrDefault()
+                                    where attribute != null
+                                    select attribute
+                                   ).ToDictionary(cua => cua.Name, cua => cua);
+
+            this.IsComplex = TypeUtility.IsComplexType(type);
+
+            bool isRoundtripEntity = TypeUtility.IsAttributeDefined(type, typeof(RoundtripOriginalAttribute), true);
+            bool hasOtherRoundtripMembers = false;
+
+            IEnumerable<PropertyInfo> properties = type.GetProperties(MemberBindingFlags)
+                .Where(p => p.GetIndexParameters().Length == 0)
+                .OrderBy(p => p.Name);
+            foreach (PropertyInfo property in properties)
+            {
+                MetaMember metaMember = new MetaMember(this, property, isRoundtripEntity);
+
+                if (metaMember.IsComplex)
+                    this.HasComplexMembers = true;
+
+                if (metaMember.IsComposition)
+                    this.HasComposition = true;
+
+                if (metaMember.RequiresValidation)
+                    this._requiresValidation = true;
+
+                if (metaMember.IsRoundtripMember)
+                {
+                    if (TypeUtility.IsAttributeDefined(property, typeof(TimestampAttribute), false) &&
+                        TypeUtility.IsAttributeDefined(property, typeof(ConcurrencyCheckAttribute), false))
+                    {
+                        // Look for a concurrency version member. There should be only one
+                        // (DomainService validation ensures this).
+                        this.VersionMember = metaMember;
+                    }
+                    else if (!metaMember.IsKeyMember)
+                    {
+                        // Look for non-key, non-timestamp roundtripped members. We can skip key members,
+                        // since they're read only and cannot be modified by the client anyways.
+                        hasOtherRoundtripMembers = true;
+                    }
+                }
+
+                this._metaMembers.Add(property.Name, metaMember);
+            }
+
+            this.ShouldRoundtripOriginal = (hasOtherRoundtripMembers || VersionMember == null);
+            if (this.HasComposition)
+            {
+                this._childTypes = _metaMembers.Values
+                        .Where(m => m.IsComposition)
+                        .Select(p => TypeUtility.GetElementType(p.PropertyType)).ToArray();
+            }
+            else
+            {
+                this._childTypes = TypeUtility.EmptyTypes;
+            }
+
+            this.Type = type;
+
+            _validationAttributes = new ReadOnlyCollection<ValidationAttribute>(this.Type.GetCustomAttributes(typeof(ValidationAttribute), true).OfType<ValidationAttribute>().ToArray());
+            _requiresValidation = _requiresValidation || _validationAttributes.Any();
+
+            // for identity purposes, we need to make sure values are always ordered
+            KeyMembers = new ReadOnlyCollection<MetaMember>(_metaMembers.Values.Where(m => m.IsKeyMember).OrderBy(m => m.Name).ToArray());
+            _dataMembers = new ReadOnlyCollection<MetaMember>(_metaMembers.Values.Where(m => m.IsDataMember).ToArray());
+
+            if (!_requiresValidation && HasComplexMembers)
+            {
+                // Reqursivly search properties on all complex members for validation attribues to 
+                // determine if validation is required
+                var visitedTypes = new HashSet<Type>() { type };
+                foreach (var member in Members)
+                {
+                    if (member.IsComplex && SearchForValidationAttributesRecursive(member.PropertyType, visitedTypes))
+                    {
+                        _requiresValidation = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the correct property binding flags to use for data members.
+        /// </summary>
+        private static BindingFlags MemberBindingFlags => BindingFlags.Instance | BindingFlags.Public;
+
+        /// <summary>
+        /// Get the <see cref="MetaMember"/> for the property with name <paramref name="memberName"/>
+        /// or <c>null</c> if no member with the name exist.
+        /// </summary>
+        public MetaMember this[string memberName]
+        {
+            get
+            {
+                MetaMember mm = null;
+                if (this._metaMembers.TryGetValue(memberName, out mm))
+                {
+                    return mm;
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="EntityActionAttribute"/> for the custom update method with the given name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>the EntityActionAttribute for the custom method; or <c>null</c> if no method was found</returns>
+        public EntityActionAttribute GetEntityAction(string name)
+        {
+            EntityActionAttribute res;
+            _customUpdateMethods.TryGetValue(name, out res);
+            return res;
+        }
+
+        /// <summary>
+        /// Gets <see cref="EntityActionAttribute" /> for all custom update method on the MetaType.
+        /// </summary>
+        /// <returns>Meta information about all entity actions on the type</returns>
+        public IEnumerable<EntityActionAttribute> GetEntityActions()
+        {
+            return _customUpdateMethods.Values;
+        }
+
+        private static Dictionary<Type, MetaType> MetaTypes
+        {
+            get
+            {
+                if (s_metaTypes == null)
+                {
+                    s_metaTypes = new Dictionary<Type, MetaType>();
+                }
+                return s_metaTypes;
+            }
+        }
+
+        /// <summary>
+        /// Gets the underlying CLR type for this MetaType
+        /// </summary>
+        public Type Type { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this is a complex type.
+        /// </summary>
+        public bool IsComplex { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this type has complex members. This does include
+        /// complex object collections.
+        /// </summary>
+        public bool HasComplexMembers { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the original should be roundtripped for this Type.
+        /// </summary>
+        public bool ShouldRoundtripOriginal { get; }
+
+        /// <summary>
+        /// Gets the concurrency version member for this type. May be null.
+        /// </summary>
+        public MetaMember VersionMember { get; }
+
+        /// <summary>
+        /// Gets the collection of all members for this Type.
+        /// </summary>
+        public IEnumerable<MetaMember> Members => this._metaMembers.Values;
+
+        /// <summary>
+        /// Gets the collection of key members for this entity Type.
+        /// The entries are sorted by Name for identity purposes.
+        /// </summary>
+        public ReadOnlyCollection<MetaMember> KeyMembers { get; }
+
+        /// <summary>
+        /// Gets the collection of data members for this Type.
+        /// </summary>
+        public IEnumerable<MetaMember> DataMembers => _dataMembers;
+
+        /// <summary>
+        /// Gets the collection of association members for this entity Type.
+        /// </summary>
+        public IEnumerable<MetaMember> AssociationMembers
+        {
+            get
+            {
+                return this._metaMembers.Values.Where(m => m.IsAssociationMember);
+            }
+        }
+
+        /// <summary>
+        /// Gets the collection of child types this entity Type composes.
+        /// </summary>
+        public IEnumerable<Type> ChildTypes => this._childTypes;
+
+        /// <summary>
+        /// Gets a value indicating whether the Type has any Type or member level
+        /// validation attributes applied. The check is recursive through any complex
+        /// type members.
+        /// </summary>
+        public bool RequiresValidation => this._requiresValidation;
+
+        /// <summary>
+        /// Gets a value indicating whether the Type has any members marked with
+        /// CompositionAttribute.
+        /// </summary>
+        public bool HasComposition { get; }
+
+        /// <summary>
+        /// Gets the Type level validation errors for the underlying Type.
+        /// </summary>
+        public IEnumerable<ValidationAttribute> ValidationAttributes => _validationAttributes;
+
+        /// <summary>
+        /// This recursive function visits every property in the type tree. For each property,
+        /// we inspect the attributes and set meta attributes as needed.
+        /// </summary>
+        /// <param name="type">The root type to calculate attributes for.</param>
+        /// <param name="visited">Visited set for recursion.</param>
+        /// <returns><c>true</c> if any nested type or property requires validation</returns>
+        private static bool SearchForValidationAttributesRecursive(Type type, HashSet<Type> visited)
+        {
+            // If already visited the type then we don't need to visit it again
+            if (!visited.Add(type))
+            {
+                return false;
+            }
+
+            // Check for type level validation
+            if (TypeUtility.IsAttributeDefined(type, typeof(ValidationAttribute), true))
+                return true;
+
+            // visit all data members
+            IEnumerable<PropertyInfo> properties = type.GetProperties(MemberBindingFlags).Where(p => p.GetIndexParameters().Length == 0).OrderBy(p => p.Name);
+            foreach (PropertyInfo property in properties)
+            {
+                if (TypeUtility.IsAttributeDefined(property, typeof(ValidationAttribute), true))
+                    return true;
+
+                // for complex members we must drill in recursively
+                if (TypeUtility.IsSupportedComplexType(property.PropertyType))
+                {
+                    Type elementType = TypeUtility.GetElementType(property.PropertyType);
+
+                    if (SearchForValidationAttributesRecursive(elementType, visited))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether entity actions for code using code gen before version 4.4.0
+        /// has been discovered.
+        /// </summary>
+        internal bool IsLegacyEntityActionsDiscovered { get; set; }
+
+        /// <summary>
+        /// Add's a EntityAction with the given name and property names.
+        /// This method is only used when discovering EntityActions for code which has used the code gen 
+        /// before version 4.4.0.
+        /// </summary>
+        /// <param name="name">The name of the entity action.</param>
+        /// <param name="canInvokePropertyName">Name of the can invoke property.</param>
+        /// <param name="isInvokedPropertyName">Name of the is invoked property.</param>
+        internal void TryAddLegacyEntityAction(string name, string canInvokePropertyName, string isInvokedPropertyName)
+        {
+            if (!_customUpdateMethods.ContainsKey(name))
+            {
+                _customUpdateMethods.Add(name, new EntityActionAttribute(name)
+                {
+                    CanInvokePropertyName = canInvokePropertyName,
+                    IsInvokedPropertyName = isInvokedPropertyName,
+                });
+            }
+        }
+    }
+}

commit b9f7fbfacc431aec97636e365b2dbeb68f02286b

Set MetaMember.IsComplex even if there is no setter
* Fix issue introduced in #129 where validation requirement was not being properly discovered
* Improve search for validation attributes by checking MetaMember information

OpenRiaServices.DomainServices.Client/Framework/Silverlight/Data/Internal/MetaMember.cs
@@ -30,9 +30,9 @@ namespace OpenRiaServices.DomainServices.Client.Internal
             bool hasGetter = (property.GetGetMethod() != null);
 
             IsCollection = TypeUtility.IsSupportedCollectionType(property.PropertyType);
+            IsComplex = TypeUtility.IsSupportedComplexType(property.PropertyType);
             if (hasGetter && (property.GetSetMethod() != null))
             {
-                IsComplex = TypeUtility.IsSupportedComplexType(property.PropertyType);
                 IsDataMember = IsComplex || TypeUtility.IsPredefinedType(property.PropertyType);
             }
 

OpenRiaServices.DomainServices.Client/Framework/Silverlight/Data/Internal/MetaType.cs
@@ -22,7 +22,7 @@ namespace OpenRiaServices.DomainServices.Client.Internal
     /// it may be cached on types elsewhere providing multithreaded access to visible properties.
     /// </remarks>
     /// </summary>
-    [DebuggerDisplay("Type = {_type.Name}")]
+    [DebuggerDisplay("Type = {Type.Name}")]
     public sealed class MetaType
     {
         /// <summary>
@@ -284,10 +284,21 @@ namespace OpenRiaServices.DomainServices.Client.Internal
         /// <returns><c>true</c> if any nested type or property requires validation</returns>
         private static bool SearchForValidationAttributesRecursive(Type type, HashSet<Type> visited)
         {
+            // For collections and similar, get the type in the collection
+            type = TypeUtility.GetElementType(type);
+
             // If already visited the type then we don't need to visit it again
             if (!visited.Add(type))
             {
                 return false;
+            }
+
+            // Check metatype cache to se if we have already
+            // determined if the type requires validation
+            MetaType metaType;
+            if (s_metaTypes.TryGetValue(type, out metaType))
+            {
+                return metaType.RequiresValidation;
             }
 
             // Check for type level validation
@@ -304,9 +315,7 @@ namespace OpenRiaServices.DomainServices.Client.Internal
                 // for complex members we must drill in recursively
                 if (TypeUtility.IsSupportedComplexType(property.PropertyType))
                 {
-                    Type elementType = TypeUtility.GetElementType(property.PropertyType);
-
-                    if (SearchForValidationAttributesRecursive(elementType, visited))
+                    if (SearchForValidationAttributesRecursive(property.PropertyType, visited))
                         return true;
                 }
             }