fb748d631e012d85304039de85f58a9c8c76b3ac
Daniel-Svensson <daniel.svensson@hotmail.se>
2018-11-12 20:21:09 +0100

    Reference msbuild 14 (VS2015) instead of msbuild 4 assemblies
    * Update TextTemplate Tests to use roslyn instead of old csc task

--- OpenRiaServices.DomainServices.Tools/Framework/ProjectSourceFileCache.cs
+++ OpenRiaServices.DomainServices.Tools/Framework/ProjectSourceFileCache.cs
@@ -5,8 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Microsoft.Build.BuildEngine;
-using Microsoft.Build.Framework;
+
 
 namespace OpenRiaServices.DomainServices.Tools
 {
