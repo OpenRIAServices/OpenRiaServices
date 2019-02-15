898935aa1b0e06aa81f661f0ca3eeb777f833286
Daniel Svensson <daniel.svensson@hotmail.se>
2019-02-15 22:48:26 +0100

    add soap based DomainClient

---
+++ OpenRiaServices.DomainServices.Client.Web/Framework/Data/SoapQueryBehavior.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace OpenRiaServices.DomainServices.Client.Web
+{
+    /// <summary>
+    /// A SOAP endpoint behavior which injects a message inspector that adds query headers.
+    /// </summary>
+    sealed class SoapEndpointBehavior : IEndpointBehavior
+    {
+        /// <summary>
+        /// Message insepctor to use if it is set to a non-<c>null</c> value.
+        /// </summary>
+        private readonly IClientMessageInspector _cookieInspector;
+        private readonly SoapQueryInspector _inspector;
+        
+        public SoapEndpointBehavior(WcfDomainClientFactory factory)
+        {
+            _inspector = new SoapQueryInspector(factory);
+            _cookieInspector = factory.SharedCookieMessageInspector;
+        }
+
+        /// <summary>
+        /// Implement to pass data at runtime to bindings to support custom behavior.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to modify.</param>
+        /// <param name="bindingParameters">The objects that binding elements require to support the behavior.</param>
+        void IEndpointBehavior.AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <summary>
+        /// Implements a modification or extension of the client across an endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint that is to be customized.</param>
+        /// <param name="clientRuntime">The client runtime to be customized.</param>
+        void IEndpointBehavior.ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+#if SILVERLIGHT
+            var inspectors = clientRuntime.MessageInspectors;
+#else
+            var inspectors = clientRuntime.ClientMessageInspectors;
+#endif
+
+            inspectors.Add(_inspector);
+            if (_cookieInspector != null)
+                inspectors.Add(_cookieInspector);
+        }
+
+        /// <summary>
+        /// Implements a modification or extension of the service across an endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint that exposes the contract.</param>
+        /// <param name="endpointDispatcher">The endpoint dispatcher to be modified or extended.</param>
+        void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <summary>
+        /// Implement to confirm that the endpoint meets some intended criteria.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to validate.</param>
+        void IEndpointBehavior.Validate(ServiceEndpoint endpoint)
+        {
+            foreach (var od in endpoint.Contract.Operations)
+            {
+                // Add FaultDescription if [FaultContractAttribute] has not already been added (by old code-generation)
+                if (!od.Faults.Any(f => f.DetailType == typeof(DomainServiceFault)))
+                {
+                    od.Faults.Add(new FaultDescription(od.Messages[0].Action + "DomainServiceFault")
+                    {
+                        DetailType = typeof(DomainServiceFault),
+                        Name = typeof(DomainServiceFault).Name,
+                        Namespace = "DomainServices",
+                    });
+                }
+            }
+            }
+    }
+}
---
+++ OpenRiaServices.DomainServices.Client.Web/Framework/Data/SoapQueryInspector.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+
+namespace OpenRiaServices.DomainServices.Client.Web
+{
+    internal sealed class SoapQueryInspector : IClientMessageInspector
+    {
+        private const string IncludeTotalCountPropertyName = "DomainServiceIncludeTotalCount";
+        private const string QueryPropertyName = "DomainServiceQuery";
+
+        private readonly WcfDomainClientFactory _factory;
+
+        public SoapQueryInspector(WcfDomainClientFactory factory)
+        {
+            _factory = factory;
+        }
+
+        private static string RewriteAction(string action)
+        {
+            // This might be better of by modifying the 
+            // endpoint during setup via behaviour instead
+            return action.Insert(action.LastIndexOf("/"), "soap");
+        }
+
+        object IClientMessageInspector.BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            // make sure the channel uses the shared cookie container
+            channel.GetProperty<IHttpCookieContainerManager>().CookieContainer =
+                _factory.CookieContainer;
+
+            request.Headers.Action = RewriteAction(request.Headers.Action);
+
+            // Add Query Options if any are set
+            var messageProperties = OperationContext.Current?.OutgoingMessageProperties;
+            if (messageProperties != null)
+            {
+                messageProperties.TryGetValue(QueryPropertyName, out object queryProperty);
+                messageProperties.TryGetValue(IncludeTotalCountPropertyName, out object includeTotalCountProperty);
+
+                // Add Query Options header if any options were specified
+                if (queryProperty != null || includeTotalCountProperty != null)
+                {
+                    var queryParts = (queryProperty != null) ? QuerySerializer.Serialize((IQueryable)queryProperty) : null;
+                    var includeTotalCount = (bool?)includeTotalCountProperty;
+
+                    //// Prepare the request message copy to be modified
+                    //MessageBuffer buffer = request.CreateBufferedCopy(int.MaxValue);
+                    //request = buffer.CreateMessage();
+                    var header = new QueryOptionsHeader(queryParts, includeTotalCount == true);
+                    request.Headers.Add(header);
+                }
+            }
+
+            return null;
+        }
+
+        void IClientMessageInspector.AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <summary>
+        /// Class represponsible for serialization of query options as a SOAP header
+        /// </summary>
+        class QueryOptionsHeader : MessageHeader
+        {
+            private const string QueryOptionElementName = "QueryOption";
+            private const string QueryNameAttribute = "Name";
+            private const string QueryValueAttribute = "Value";
+            private const string QueryIncludeTotalCountOption = "includeTotalCount";
+            private const string QueryHeaderName = "DomainServiceQuery";
+            private const string QueryHeaderNamespace = "DomainServices";
+
+            private readonly IEnumerable<ServiceQueryPart> _queryParts;
+            private readonly bool _includeTotalCount;
+
+            public QueryOptionsHeader(IEnumerable<ServiceQueryPart> queryParts, bool includeTotalCount)
+            {
+                _queryParts = queryParts;
+                _includeTotalCount = includeTotalCount;
+            }
+
+            public override string Name => QueryHeaderName;
+
+            public override string Namespace => QueryHeaderNamespace;
+
+            protected override void OnWriteHeaderContents(System.Xml.XmlDictionaryWriter writer, MessageVersion messageVersion)
+            {
+                if (_queryParts != null)
+                {
+                    foreach (var part in _queryParts)
+                    {
+                        writer.WriteStartElement(QueryOptionElementName);
+                        writer.WriteAttributeString(QueryNameAttribute, part.QueryOperator);
+                        writer.WriteAttributeString(QueryValueAttribute, part.Expression);
+                        writer.WriteEndElement();
+                    }
+                }
+
+                if (_includeTotalCount == true)
+                {
+                    writer.WriteStartElement(QueryOptionElementName);
+                    writer.WriteAttributeString(QueryNameAttribute, QueryIncludeTotalCountOption);
+                    writer.WriteAttributeString(QueryValueAttribute, "true");
+                    writer.WriteEndElement();
+                }
+            }
+        }
+    }
+}
