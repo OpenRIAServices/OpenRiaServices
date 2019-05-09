16846e3f6c9543b0c8839d2fb3e4ad46e9b6c9aa
Daniel Svensson <daniel.svensson@hotmail.se>
2019-05-09 14:29:37 +0200

    fix build errors for sserver assembly on netstandard

--- src/OpenRiaServices.Server.Authentication.AspNetMembership/Framework/AuthenticationBase.cs
+++ src/OpenRiaServices.Server.Authentication.AspNetMembership/Framework/AuthenticationBase.cs
@@ -40,7 +40,7 @@ namespace OpenRiaServices.Server.Authentication.AspNetMembership
     {
         #region Static fields
 
-        private static readonly IEnumerable<string> DefaultRoles = Array.Empty<string>();
+        private static readonly string[] DefaultRoles = Array.Empty<string>();
         private static readonly IPrincipal DefaultPrincipal =
             new GenericPrincipal(new GenericIdentity(string.Empty), DefaultRoles.ToArray());
 
