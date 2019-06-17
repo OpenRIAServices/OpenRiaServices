19061fc50953bca1b971c88865a3e37ecee75e82
Daniel Svensson <daniel.svensson@hotmail.se>
2019-06-17 12:56:42 +0200

    Start with async implementation for server hosting

--- src/OpenRiaServices.DomainServices.Hosting/Framework/Services/Behaviors/DomainOperationInvoker.cs
+++ src/OpenRiaServices.DomainServices.Hosting/Framework/Services/Behaviors/DomainOperationInvoker.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 using System.ServiceModel;
 using System.ServiceModel.Dispatcher;
+using System.Threading;
+using System.Threading.Tasks;
 using OpenRiaServices.DomainServices.Server;
 
 namespace OpenRiaServices.DomainServices.Hosting
@@ -9,6 +11,12 @@ namespace OpenRiaServices.DomainServices.Hosting
     internal abstract class DomainOperationInvoker : IOperationInvoker
     {
         private readonly DomainOperationType operationType;
+
+        protected struct InvokeResult
+        {
+            public object result;
+            public object[] outputs;
+        }
 
         public DomainOperationInvoker(DomainOperationType operationType)
         {
@@ -19,7 +27,7 @@ namespace OpenRiaServices.DomainServices.Hosting
         {
             get
             {
-                return true;
+                return false;
             }
         }
 
@@ -33,21 +41,20 @@ namespace OpenRiaServices.DomainServices.Hosting
         public object Invoke(object instance, object[] inputs, out object[] outputs)
         {
             string operationName = this.Name;
-            long startTicks = DateTime.UtcNow.Ticks;
-            object result = null;
-            DomainService domainService = null;
+            long startTicks = DiagnosticUtility.GetTicks();
             try
             {
                 DiagnosticUtility.OperationInvoked(operationName);
 
-                domainService = this.GetDomainService(instance);
+                var domainService = this.GetDomainService(instance);
 
                 // invoke the operation and process the result
                 this.ConvertInputs(inputs);
-                result = this.InvokeCore(domainService, inputs, out outputs);
+                var result = this.InvokeCore(domainService, inputs, out outputs);
                 result = this.ConvertReturnValue(result);
 
                 DiagnosticUtility.OperationCompleted(operationName, DiagnosticUtility.GetDuration(startTicks));
+                return result;
             }
             catch (FaultException)
             {
@@ -70,8 +77,6 @@ namespace OpenRiaServices.DomainServices.Hosting
                 // fault exception.
                 throw ServiceUtility.CreateFaultException(ex);
             }
-
-            return result;
         }
 
         protected virtual void ConvertInputs(object[] inputs)
@@ -83,16 +88,72 @@ namespace OpenRiaServices.DomainServices.Hosting
             return returnValue;
         }
 
-        protected abstract object InvokeCore(object instance, object[] inputs, out object[] outputs);
+        protected virtual object InvokeCore(object instance, object[] inputs, out object[] outputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual Task<InvokeResult> InvokeCoreAsync(object instance, object[] inputs)
+        {
+            InvokeResult invokeResult;
+            invokeResult.result = InvokeCore(instance, inputs, out invokeResult.outputs);
+            return Task.FromResult(invokeResult);
+        }
 
         public IAsyncResult InvokeBegin(object instance, object[] inputs, AsyncCallback callback, object state)
         {
-            throw new NotSupportedException();
+            return TaskExtensions.BeginApm(InvokeAsync(instance, inputs), callback, state);
         }
 
         public object InvokeEnd(object instance, out object[] outputs, IAsyncResult result)
         {
-            throw new NotSupportedException();
+            var invokeResult = TaskExtensions.EndApm<InvokeResult>(result);
+
+            outputs = invokeResult.outputs;
+            return invokeResult.result;
+        }
+
+        private async Task<InvokeResult> InvokeAsync(object instance, object[] inputs)
+        {
+            string operationName = this.Name;
+            long startTicks = DiagnosticUtility.GetTicks();
+
+            try
+            {
+                DiagnosticUtility.OperationInvoked(operationName);
+
+                DomainService domainService = this.GetDomainService(instance);
+
+                // invoke the operation and process the result
+                this.ConvertInputs(inputs);
+                var invokeResult = await this.InvokeCoreAsync(domainService, inputs).ConfigureAwait(false);
+                invokeResult.result = this.ConvertReturnValue(invokeResult.result);
+
+                DiagnosticUtility.OperationCompleted(operationName, DiagnosticUtility.GetDuration(startTicks));
+
+                return invokeResult;
+            }
+            catch (FaultException)
+            {
+                DiagnosticUtility.OperationFaulted(operationName, DiagnosticUtility.GetDuration(startTicks));
+
+                // if the exception has already been transformed to a fault
+                // just rethrow it
+                throw;
+            }
+            catch (Exception ex)
+            {
+                if (ex.IsFatal())
+                {
+                    throw;
+                }
+                DiagnosticUtility.OperationFailed(operationName, DiagnosticUtility.GetDuration(startTicks));
+
+                // We need to ensure that any time an exception is thrown by the
+                // service it is transformed to a properly sanitized/configured
+                // fault exception.
+                throw ServiceUtility.CreateFaultException(ex);
+            }
         }
 
         private DomainService GetDomainService(object instance)
---
+++ src/OpenRiaServices.DomainServices.Hosting/Framework/Services/TaskExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRiaServices.DomainServices.Hosting
+{
+    static class TaskExtensions
+    {
+        /// <summary>
+        /// Helper method to convert from Task async method to "APM" (IAsyncResult with Begin/End calls)
+        /// Copied from 
+        /// https://docs.microsoft.com/en-us/dotnet/standard/asynchronous-programming-patterns/interop-with-other-asynchronous-patterns-and-types#TapToApm
+        /// </summary>
+        public static IAsyncResult BeginApm<T>(Task<T> task,
+                                    AsyncCallback callback,
+                                    object state)
+        {
+            var tcs = new TaskCompletionSource<T>(state);
+            task.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                    tcs.TrySetException(t.Exception.InnerExceptions);
+                else if (t.IsCanceled)
+                    tcs.TrySetCanceled();
+                else
+                    tcs.TrySetResult(t.Result);
+
+                callback?.Invoke(tcs.Task);
+            }, TaskScheduler.Default);
+            return tcs.Task;
+        }
+
+        public static T EndApm<T>(IAsyncResult asyncResult)
+        {
+            return ((Task<T>)asyncResult).GetAwaiter().GetResult();
+        }
+    }
+}
