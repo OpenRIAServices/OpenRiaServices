08ae8b0ef9ce7ca2f5b02804b2309dcb3df7693f
Daniel Svensson <daniel.svensson@hotmail.se>
2019-06-24 20:40:27 +0200

    move files for client tests
    * remove some old "Silverlight" folders and move code to respecive test

--- OpenRiaServices.DomainServices.Client/Test/Desktop/Platforms/net45/CodegenWorkaround.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OpenRiaServices.DomainServices.Client.ApplicationServices
-{
-    /// <summary>
-    /// This class just exists so files generated to use the OpenRiaServices.DomainServices.Client.ApplicationServices
-    /// namespace will compile on the desktop.
-    /// </summary>
-    public class CodegenWorkaround { }
-}
