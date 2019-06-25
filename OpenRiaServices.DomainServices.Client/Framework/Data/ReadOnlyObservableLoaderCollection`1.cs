6b6b42538ee7e0d32590669a64a9b1214ce13f6b
Daniel Svensson <daniel.svensson@hotmail.se>
2019-06-25 19:37:29 +0200

    add specialised collection

---
+++ OpenRiaServices.DomainServices.Client/Framework/Data/ReadOnlyObservableLoaderCollection`1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Collections.Specialized;
+
+namespace OpenRiaServices.DomainServices.Client.Data
+{
+    /// <summary>
+    /// Class used for representing resettable collections used to optimize <see cref="LoadOperation"/> collections.
+    /// It is similar to <see cref="ReadOnlyObservableCollection{T}"/> but allows changing the contents when a load has happened.
+    /// </summary>
+    /// <typeparam name="T">type of entity</typeparam>
+    internal class ReadOnlyObservableLoaderCollection<T> : ReadOnlyCollection<T>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        public ReadOnlyObservableLoaderCollection()
+            : base(new List<T>())
+        {
+
+        }
+
+        public ReadOnlyObservableLoaderCollection(IEnumerable<T> list)
+            : base(new List<T>(list))
+        {
+
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Reset(IEnumerable<T> collection)
+        {
+            var list = (List<T>)base.Items;
+
+            list.Clear();
+            list.AddRange(collection);
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Count)));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
