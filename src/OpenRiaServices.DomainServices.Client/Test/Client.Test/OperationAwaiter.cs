abee3d150c5360c30be5ad748489ce203e10374e
Daniel Svensson <daniel.svensson@hotmail.se>
2019-07-01 13:37:56 +0200

    Add custom awaiter for tests

---
+++ src/OpenRiaServices.DomainServices.Client/Test/Client.Test/OperationAwaiter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace OpenRiaServices.DomainServices.Client
+{
+    public static class OperationExtensions
+    {
+        public static OperationAwaiter GetAwaiter(this OperationBase operation)
+        {
+            return new OperationAwaiter(operation);
+        }
+
+        public struct OperationAwaiter : INotifyCompletion
+        {
+            private readonly OperationBase _operation;
+            public OperationAwaiter(OperationBase control)
+            {
+                _operation = control;
+            }
+            public bool IsCompleted
+            {
+                get { return _operation.IsComplete; }
+            }
+
+            public void OnCompleted(Action continuation)
+            {
+                _operation.Completed += (sender, args) => continuation();
+            }
+
+            public void GetResult()
+            {
+                if (!_operation.IsComplete)
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
