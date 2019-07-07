ac25927344113b2bb48d7fd459613fb576b23ad7
Daniel Svensson <daniel.svensson@hotmail.se>
2019-07-07 20:03:59 +0200

    extract task portability work to separate file

---
+++ OpenRiaServices.DomainServices.Client/Framework/Platforms/sl5/TaskPortabilityExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Threading.Tasks
+{
+    internal static class TaskPortabilityExtensions
+    {
+        public static TaskAwaiter<T> GetAwaiter<T>(this Task<T> task)
+        {
+            return new TaskAwaiter<T>() { Task = task };
+        }
+
+        public static Task ContinueWith<T>(this Task<T> task, Action<Task<T>, object> action,
+            object state, CancellationToken ct, TaskContinuationOptions tco, TaskScheduler taskScheduler)
+        {
+            Action<Task<T>> callback = (t) => action(t, state);
+            return task.ContinueWith(callback, ct, tco, taskScheduler);
+        }
+
+        public static Task<TRes> ContinueWith<T, TRes>(this Task<T> task, Func<Task<T>, object, TRes> func,
+            object state, CancellationToken ct, TaskContinuationOptions tco, TaskScheduler taskScheduler)
+        {
+            Func<Task<T>, TRes> callback = (t) => func(t, state);
+            return task.ContinueWith(callback, ct, tco, taskScheduler);
+        }
+
+        public struct TaskAwaiter<T>
+        {
+            public Task<T> Task;
+
+            public T GetResult()
+            {
+                if (Task.IsFaulted)
+                    throw UnwrapException(Task.Exception);
+                return Task.Result;
+            }
+
+            private Exception UnwrapException(AggregateException exception)
+            {
+                while (exception.InnerException is AggregateException aggregateException)
+                    exception = aggregateException;
+
+                return exception.InnerException;
+            }
+        }
+    }
+}
--- OpenRiaServices.DomainServices.Client/Framework/PortabilityExtensions.cs
+++ OpenRiaServices.DomainServices.Client/Framework/PortabilityExtensions.cs
@@ -12,32 +12,5 @@ namespace OpenRiaServices.DomainServices.Client
         {
             return new ReadOnlyCollection<T>(list);
         }
-
-#if SILVERLIGHT
-        public struct TaskAwaiter<T>
-        {
-            public Task<T> Task;
-
-            public T GetResult()
-            {
-                if (Task.IsFaulted)
-                    throw UnwrapException(Task.Exception);
-                return Task.Result;
-            }
-
-            private Exception UnwrapException(AggregateException exception)
-            {
-                while (exception.InnerException is AggregateException aggregateException)
-                    exception = aggregateException;
-
-                return exception.InnerException;
-            }
-        }
-
-        public static TaskAwaiter<T> GetAwaiter<T>(this Task<T> task)
-        {
-            return new TaskAwaiter<T>() { Task = task };
-        }
-#endif
     }
 }
