e9e36b330a1ef16e6fc4151f25f533678fd352a2
Daniel Svensson <daniel.svensson@hotmail.se>
2019-08-20 21:58:01 +0200

    Make CompositionDelete test more deterministic (#172)
    * Make CompositionDelete test more deterministic
      * Setup the data to be deleted so that not all rows gets removed and tests starts to fail as a result
    * refactor out webserver code for easier reuse and use hepler in odata tests to

--- OpenRiaServices.DomainServices.Client/Test/Desktop/Platforms/net45/Main.cs
+++ OpenRiaServices.DomainServices.Client/Test/Desktop/Platforms/net45/Main.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Management;
 using System.Net;
 using System.Text;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenRiaServices.Common.Test;
 
 namespace OpenRiaServices.DomainServices.Client.Test
 {
     [TestClass()]
     public sealed class Main
     {
-        private static Process s_iisProcess = null;
+        private static IISExpressWebserver s_webServer = null;
 
         [AssemblyInitialize()]
         public static void AssemblyInit(TestContext context)
@@ -40,31 +39,7 @@ namespace OpenRiaServices.DomainServices.Client.Test
             // have been run
             ((IDisposable)UpdateTests.TestDatabase).Dispose();
 
-            StopWebserver();
-        }
-
-        private static string GetCommandLine(Process processs)
-        {
-            ManagementObjectSearcher commandLineSearcher = new ManagementObjectSearcher(
-                $"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {processs.Id}");
-            foreach (ManagementObject commandLineObject in commandLineSearcher.Get())
-            {
-                return commandLineObject["CommandLine"]?.ToString() ?? string.Empty;
-            }
-
-            return string.Empty;
-        }
-
-
-        private static bool IsWebServerRunning(string commandlineArguments)
-        {
-            foreach(var process in Process.GetProcessesByName("iisexpress"))
-            {
-                var commandline = GetCommandLine(process);
-                if (commandline.Contains(commandlineArguments))
-                    return true;
-            }
-            return false;
+            s_webServer?.Stop();
         }
 
         private static void StartWebServer()
@@ -75,67 +50,8 @@ namespace OpenRiaServices.DomainServices.Client.Test
             if (!Directory.Exists(webSitePath))
                 throw new FileNotFoundException($"Website not found at {webSitePath}");
 
-            string iisexpress = System.Environment.ExpandEnvironmentVariables(@"%programfiles%\IIS Express\iisexpress.exe");
-
-            string portNumberText = "60002";
-            string arguments = $"/port:{portNumberText} /path:\"{webSitePath}\"";
-
-            // only start webserver if it is not already running
-            if (!IsWebServerRunning(arguments))
-            {
-                var startInfo = new ProcessStartInfo();
-                startInfo.FileName = iisexpress;
-                startInfo.Arguments = arguments;
-                startInfo.RedirectStandardInput = true;
-                startInfo.RedirectStandardOutput = true;
-                startInfo.UseShellExecute = false;
-                startInfo.CreateNoWindow = true;
-
-                s_iisProcess = Process.Start(startInfo);
-
-                // Wait for IIS start
-                string str;
-                while ((str = s_iisProcess.StandardOutput.ReadLine()) != null)
-                {
-                    if (str.Contains("IIS Express is running")
-                        || str.Contains("'Q'"))
-                        return;
-                }
-
-                throw new Exception("Failed to start IIS express");
-            }
-        }
-
-        /// <summary>Performs cleanup and ensures that there are no active web servers.</summary>
-        public static void StopWebserver()
-        {
-            if (s_iisProcess != null)
-            {
-                // The local web server does not respond to CloseMainWindow.
-                Trace.WriteLine("Closing web server process...");
-
-                if (!s_iisProcess.HasExited)
-                {
-                    try
-                    {
-                        s_iisProcess.StandardInput.WriteLine("Q");
-                        s_iisProcess.WaitForExit(1 * 1000); // wait 1 secs.
-
-                        if (!s_iisProcess.HasExited)
-                        {
-                            s_iisProcess.Kill();
-                            s_iisProcess.WaitForExit(60 * 1000); // wait 60 secs.
-                        }
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        Trace.WriteLine("Unable to kill local web server process.");
-                    }
-                }
-
-                s_iisProcess.Dispose();
-                s_iisProcess = null;
-            }
+            s_webServer = new IISExpressWebserver();
+            s_webServer.Start(webSitePath, 60002);
         }
     }
 }
---
+++ Test/Desktop/OpenRiaServices.Common.Test/Platforms/net45/IISExpressWebserver.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Management;
+using System.Security;
+using System.Threading;
+
+namespace OpenRiaServices.Common.Test
+{
+    public class IISExpressWebserver
+    {
+        Process _iisProcess;
+
+        public bool Start(string path, int port)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+            string siteName = Path.GetFileName(path);
+            if (string.IsNullOrEmpty(siteName))
+                throw new ArgumentException("path cannot end with directory separator", nameof(path));
+            if (_iisProcess != null)
+                throw new InvalidOperationException("server already started");
+
+            string iisexpress = Environment.ExpandEnvironmentVariables(@"%programfiles%\IIS Express\iisexpress.exe");
+
+            string portNumberText = port.ToString(CultureInfo.InvariantCulture);
+            string arguments = $"/port:{portNumberText} /path:\"{path}\"";
+
+            // only start webserver if it is not already running
+            if (!ShouldStartNewWebserverProcess(arguments, $"/site:\"{siteName}\""))
+            {
+                return false;
+            }
+
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = iisexpress;
+            startInfo.Arguments = arguments;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+
+            _iisProcess = Process.Start(startInfo);
+
+            string str;
+            while ((str = _iisProcess.StandardOutput.ReadLine()) != null)
+            {
+                if (str.Contains("IIS Express is running")
+                    || str.Contains("'Q'"))
+                {
+                    new Thread(ProcessIISStandardOutput)
+                            .Start();
+
+                    return true;
+                }
+
+            }
+
+            throw new Exception("Failed to start IIS express");
+        }
+
+        // Read and discard output from the web server so that 
+        // it does not fill upp the standard output buffer and 
+        // pause for it to be read
+        private void ProcessIISStandardOutput()
+        {
+            try
+            {
+                string str;
+                while (!_iisProcess.HasExited &&
+                    (str = _iisProcess.StandardOutput.ReadLine()) != null)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+                if (!_iisProcess.HasExited)
+                    throw;
+            }
+        }
+
+        /// <summary>Performs cleanup and ensures that there are no active web servers.</summary>
+        public void Stop()
+        {
+            if (_iisProcess != null)
+            {
+                if (!_iisProcess.HasExited)
+                {
+                    try
+                    {
+                        _iisProcess.StandardInput.WriteLine("Q");
+                        _iisProcess.WaitForExit(1 * 1000); // wait 1 secs.
+
+                        if (!_iisProcess.HasExited)
+                        {
+                            _iisProcess.Kill();
+                            _iisProcess.WaitForExit(60 * 1000); // wait 60 secs.
+                        }
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Trace.WriteLine("Unable to kill local web server process.");
+                    }
+                }
+
+                _iisProcess.Dispose();
+                _iisProcess = null;
+            }
+        }
+
+        [SecuritySafeCritical]
+        private static string GetCommandLine(Process processs)
+        {
+            var commandLineSearcher = new ManagementObjectSearcher(
+                $"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {processs.Id}");
+            foreach (ManagementObject commandLineObject in commandLineSearcher.Get())
+            {
+                return commandLineObject["CommandLine"]?.ToString() ?? string.Empty;
+            }
+
+            return string.Empty;
+        }
+
+        private static bool ShouldStartNewWebserverProcess(string commandlineArguments, string dontKillCommandLine)
+        {
+            foreach (var process in Process.GetProcessesByName("iisexpress"))
+            {
+                var commandline = GetCommandLine(process);
+                if (commandline.Contains(commandlineArguments))
+                {
+                    // Process was left from another test session.
+                    // Kill it so we can start a new with redirected intput/output
+                    process.Kill();
+                    process.WaitForExit();
+                    return true;
+                }
+                else if (commandline.Contains(dontKillCommandLine))
+                {
+                    // return true and do nothing if different command line (started by user/vs)
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
