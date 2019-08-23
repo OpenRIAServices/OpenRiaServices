13ac81b1f50039efd5397cde15e78a0659b33f0f
Daniel Svensson <daniel.svensson@hotmail.se>
2019-08-23 12:55:49 +0200

    add readme to archive folder

--- src/archive/RiaServicesContrib.DomainServices.EntityFramework/Test/DbContextModel/AdventureWorks/AdventureWorksDbCtx.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\dd\Alex_AppFx_1\src\AppFx\RiaServices\Main\Microsoft.ServiceModel.DomainServices.EntityFramework\Test\DbContextModel\AdventureWorks\AdventureWorksDbCtx.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
