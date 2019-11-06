46fd947cfa7e3e74fb59bc334a5b6c7794e3c2fc
Daniel Svensson <daniel.svensson@hotmail.se>
2019-11-06 23:01:33 +0100

    Fix problems with await in tests (#208)
    Improve "async/await" implementations for operations by propagating exceptions from callbacks to await point
    disable diagnostics from tests by default

--- src/OpenRiaServices.DomainServices.Client/Test/Client.Test/OperationAwaiter.cs
+++ src/OpenRiaServices.DomainServices.Client/Test/Client.Test/OperationAwaiter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
@@ -10,14 +11,18 @@ namespace OpenRiaServices.DomainServices.Client
 {
     public static class OperationExtensions
     {
+        private static readonly Func<Task> s_getCurrentTask = GetCurrentTaskGetter();
+
         public static OperationAwaiter GetAwaiter(this OperationBase operation)
         {
             return new OperationAwaiter(operation);
         }
 
-        public struct OperationAwaiter : INotifyCompletion
+        public class OperationAwaiter : INotifyCompletion
         {
             private readonly OperationBase _operation;
+            private Task _domainContextCompletionTask;
+
             public OperationAwaiter(OperationBase control)
             {
                 _operation = control;
@@ -29,9 +34,47 @@ namespace OpenRiaServices.DomainServices.Client
 
             public void OnCompleted(Action continuation)
             {
+                // Capture syncContext and scheduler from await location
+                var syncContext = SynchronizationContext.Current;
+                var scheduler = TaskScheduler.Current;
+                var executionContext = ExecutionContext.Capture();
+
+                ContextCallback action;
+                if (syncContext is null /*|| syncContext is Test.TestSynchronizationContext*/)
+                {
+                    action = (object o) => ((Action)o)();
+                }
+                else
+                {
+                    action = (object o) =>
+                    {
+                        SynchronizationContext.Current.Post((object s) => { ((Action)s)(); }, o);
+                    };
+                }
+
                 _operation.Completed += (sender, args) =>
                 {
-                    ThreadPool.QueueUserWorkItem((object state) => ((Action)state)(), continuation);
+                    // The operation is completed in a ContinueWith callback
+                    // Get the task associated with the callback
+                    // so that continuation is run after the whole completion
+                    // Since the currentTask is currently executing
+                    // This will not complete now, but later when it is finished
+                    _domainContextCompletionTask = s_getCurrentTask();
+                    if (_domainContextCompletionTask != null)
+                    {
+                        _domainContextCompletionTask.ContinueWith((Task _, object o) =>
+                        {
+                            ExecutionContext.Run(executionContext, action, o);
+                        }
+                        , continuation
+                        , CancellationToken.None
+                        , TaskContinuationOptions.None
+                        , scheduler);
+                    }
+                    else
+                    {
+                        ExecutionContext.Run(executionContext, action, continuation);
+                    }
                 };
             }
 
@@ -39,7 +82,20 @@ namespace OpenRiaServices.DomainServices.Client
             {
                 if (!_operation.IsComplete)
                     throw new InvalidOperationException();
+
+                // Pass any exception from the callbacks which were not handled
+                if (_domainContextCompletionTask != null)
+                    _domainContextCompletionTask.GetAwaiter().GetResult();
             }
+        }
+
+        private static Func<Task> GetCurrentTaskGetter()
+        {
+            var property =
+typeof(Task).GetProperty("InternalCurrent", BindingFlags.Static | BindingFlags.NonPublic);
+
+            var getMethod = property.GetGetMethod(nonPublic: true);
+            return (Func<Task>)Delegate.CreateDelegate(typeof(Func<Task>), getMethod);
         }
     }
 }
