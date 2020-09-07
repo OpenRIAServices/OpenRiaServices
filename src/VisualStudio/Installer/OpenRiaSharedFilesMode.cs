47f253f3a48674fa9a320d2c1e4f83674a8eb234
Daniel-Svensson <daniel.svensson@hotmail.se>
2020-09-07 10:31:58 +0200

    Add xml comments to OpenRiaSharedFilesMode

--- src/VisualStudio/Installer/OpenRiaSharedFilesMode.cs
+++ src/VisualStudio/Installer/OpenRiaSharedFilesMode.cs
@@ -1,9 +1,19 @@
 ﻿namespace OpenRiaServices.VisualStudio.Installer
 {
+    /// <summary>
+    /// Determine how ".shared" files should be handled by code generation
+    /// </summary>
     public enum OpenRiaSharedFilesMode
     {
-        Link,
-        Copy,
+        /// <summary>
+        /// Reference the server version of the file (same as "Add existing file as link")
+        /// </summary>
+        Link = 0,
+
+        /// <summary>
+        /// Copy all shared files and reference copy
+        /// </summary>
+        Copy = 1,
     }
 
 }
