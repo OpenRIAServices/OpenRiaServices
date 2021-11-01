8a91c929acd527250e5ea564d067d57050fda55f
Hugo Lindell <hugo.lindell@crm.se>
2021-11-01 10:38:59 +0100

    Namespace rename

--- src/OpenRiaServices.Server.EntityFrameworkCore/Test/DbContextModel/AdventureWorks/SchaffoldedDBContext/AdventureWorksContext.cs
+++ src/OpenRiaServices.Server.EntityFrameworkCore/Test/DbContextModel/AdventureWorks/SchaffoldedDBContext/AdventureWorksContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 // If you have enabled NRTs for your project, then un-comment the following line:
 // #nullable disable
 
-namespace DbContextModels
+namespace DbContextModels.AdventureWorksEFCoreScaffolded
 {
     public partial class AdventureWorksContext : DbContext
     {
--- src/OpenRiaServices.Server.EntityFrameworkCore/Test/DbContextModel/AdventureWorks/SchaffoldedDBContext/TransactionHistoryArchive.cs
+++ src/OpenRiaServices.Server.EntityFrameworkCore/Test/DbContextModel/AdventureWorks/SchaffoldedDBContext/TransactionHistoryArchive.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 // If you have enabled NRTs for your project, then un-comment the following line:
 // #nullable disable
 
-namespace DbContextModels
+namespace DbContextModels.AdventureWorksEFCoreScaffolded
 {
     public partial class TransactionHistoryArchive
     {
