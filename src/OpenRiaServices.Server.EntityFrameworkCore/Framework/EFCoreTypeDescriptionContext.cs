ddd4f5108670787c1ab4cc03fa6eb0b79e217c92
Hugo Lindell <hugo.lindell@crm.se>
2022-04-04 10:57:31 +0200

    Remove unused methods.

--- src/OpenRiaServices.Server.EntityFrameworkCore/Framework/EFCoreTypeDescriptionContext.cs
+++ src/OpenRiaServices.Server.EntityFrameworkCore/Framework/EFCoreTypeDescriptionContext.cs
@@ -47,43 +47,6 @@ namespace OpenRiaServices.Server.EntityFrameworkCore
 
         public IEntityType GetEntityType(Type type) => Model.FindEntityType(type);
 
-        ///// <summary>
-        ///// Returns the association information for the specified navigation property.
-        ///// </summary>
-        ///// <param name="navigationProperty">The navigation property to return association information for</param>
-        ///// <returns>The association info</returns>
-        //internal AssociationInfo GetAssociationInfo(NavigationProperty navigationProperty)
-        //{
-        //    lock (this._associationMap)
-        //    {
-        //        string associationName = navigationProperty.RelationshipType.FullName;
-        //        AssociationInfo associationInfo = null;
-        //        if (!this._associationMap.TryGetValue(associationName, out associationInfo))
-        //        {
-        //            AssociationType associationType = (AssociationType)navigationProperty.RelationshipType;
-
-        //            if (!associationType.ReferentialConstraints.Any())
-        //            {
-        //                // We only support EF models where FK info is part of the model.
-        //                throw new NotSupportedException(
-        //                    string.Format(CultureInfo.CurrentCulture,
-        //                    ResourceEFCore.LinqToEntitiesProvider_UnableToRetrieveAssociationInfo, associationName));
-        //            }
-
-        //            associationInfo = new AssociationInfo();
-        //            associationInfo.FKRole = associationType.ReferentialConstraints[0].ToRole.Name;
-        //            associationInfo.Name = this.GetAssociationName(navigationProperty, associationInfo.FKRole);
-        //            associationInfo.ThisKey = associationType.ReferentialConstraints[0].ToProperties.Select(p => p.Name).ToArray();
-        //            associationInfo.OtherKey = associationType.ReferentialConstraints[0].FromProperties.Select(p => p.Name).ToArray();
-        //            associationInfo.IsRequired = associationType.RelationshipEndMembers[0].RelationshipMultiplicity == RelationshipMultiplicity.One;
-
-        //            this._associationMap[associationName] = associationInfo;
-        //        }
-
-        //        return associationInfo;
-        //    }
-        //}
-
         /// <summary>
         /// Creates an AssociationAttribute for the specified navigation property
         /// </summary>
@@ -92,13 +55,6 @@ namespace OpenRiaServices.Server.EntityFrameworkCore
         internal AssociationAttribute CreateAssociationAttribute(INavigation navigationProperty)
         {
             var fk = navigationProperty.ForeignKey;
-
-
-            ////                // We only support EF models where FK info is part of the model.
-            //if (fk is null)
-            //    throw new NotSupportedException(
-            //        string.Format(CultureInfo.CurrentCulture,
-            //        ResourceEFCore.LinqToEntitiesProvider_UnableToRetrieveAssociationInfo, associationName));
 
             string thisKey;
             string otherKey;
@@ -119,35 +75,5 @@ namespace OpenRiaServices.Server.EntityFrameworkCore
             assocAttrib.IsForeignKey = navigationProperty.IsDependentToPrincipal(); // TODO:  isForeignKey;
             return assocAttrib;
         }
-
-        /// <summary>
-        /// Returns a unique association name for the specified navigation property.
-        /// </summary>
-        /// <param name="navigationProperty">The navigation property</param>
-        /// <param name="foreignKeyRoleName">The foreign key role name for the property's association</param>
-        /// <returns>A unique association name for the specified navigation property.</returns>
-        /*
-        private string GetAssociationName(NavigationProperty navigationProperty, string foreignKeyRoleName)
-        {
-            RelationshipEndMember fromMember = navigationProperty.FromEndMember;
-            RelationshipEndMember toMember = navigationProperty.ToEndMember;
-
-            RefType toRefType = toMember.TypeUsage.EdmType as RefType;
-            EntityType toEntityType = toRefType.ElementType as EntityType;
-
-            RefType fromRefType = fromMember.TypeUsage.EdmType as RefType;
-            EntityType fromEntityType = fromRefType.ElementType as EntityType;
-
-            bool isForeignKey = navigationProperty.FromEndMember.Name == foreignKeyRoleName;
-            string fromTypeName = isForeignKey ? fromEntityType.Name : toEntityType.Name;
-            string toTypeName = isForeignKey ? toEntityType.Name : fromEntityType.Name;
-
-            // names are always formatted non-FK side type name followed by FK side type name
-            string associationName = string.Format(CultureInfo.InvariantCulture, "{0}_{1}", toTypeName, fromTypeName);
-            associationName = MakeUniqueName(associationName, this._associationMap.Values.Select(p => p.Name));
-
-            return associationName;
-        }
-        */
     }
 }
