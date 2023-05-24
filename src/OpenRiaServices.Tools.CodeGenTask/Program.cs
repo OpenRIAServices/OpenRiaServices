7c7bf4a4f1c7371fe5074357990e492645dad2f0
Hugo Lindell <hugo.lindell@crm.se>
2023-05-24 11:18:34 +0200

    Add codegen project

---
+++ src/OpenRiaServices.Tools.CodeGenTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using System.IO;
+using OpenRiaServices.Tools;
+using System.Linq;
+
+public class CodeGenTask
+{    static void Main(string[] args) // Take in params to setup the build here
+     {
+        // Setup the codegen task based on the parameters
+        var codeGenTask = new CreateOpenRiaClientFilesTask();
+        codeGenTask.BuildEngine = null; // TODO
+        codeGenTask.Language = "C#"; // Take in to decide if we should use vb or C#
+        codeGenTask.ServerProjectPath = ""; // TODO
+
+        // Run the codegen
+        var success = codeGenTask.Execute();
+        if (success)
+        {
+            Console.WriteLine("Code generation succeeded");
+            Console.WriteLine($"Generated code can be found at: {codeGenTask.GeneratedCodePath}");
+        }
+        else
+        {
+            Console.WriteLine("Code generation failed");
+        }
+    }
+}
