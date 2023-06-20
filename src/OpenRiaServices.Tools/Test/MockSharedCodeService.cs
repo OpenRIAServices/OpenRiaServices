37e0d9f314002a6d1ef805a5f99076f2abc3910a
Daniel Svensson <daniel.svensson@crm.se>
2023-06-20 09:46:47 +0200

    Improve MockSharedCodeServcie

--- src/OpenRiaServices.Tools/Test/MockSharedCodeService.cs
+++ src/OpenRiaServices.Tools/Test/MockSharedCodeService.cs
@@ -261,6 +261,12 @@ namespace OpenRiaServices.Tools.Test
                 return false;
             }
 
+            // We should check int idx = assemblyFullName.IndexOf("PublicKeyToken=", StringComparison.OrdinalIgnoreCase)
+            // To handle more types, but this should fix a large part of the system types
+            if (t.Assembly == typeof(object).Assembly
+                || t.Assembly == typeof(DisplayAttribute).Assembly)
+                return true;
+
             // Otherwise, we check for specific namespaces and types
             return
                 !string.IsNullOrEmpty(t.Namespace) &&
