286c839ba4203cd9dd3a565e9c1077ec0d976bbd
Erik Sandström <116262628+SandstromErik@users.noreply.github.com>
2023-08-01 21:22:46 +0200

    Fix insecure temporary file creation (#429)
    * Fix insecure temporary file creation reported by code analysis

--- src/OpenRiaServices.Tools/Framework/CreateOpenRiaClientFilesTask.cs
+++ src/OpenRiaServices.Tools/Framework/CreateOpenRiaClientFilesTask.cs
@@ -785,11 +785,9 @@ namespace OpenRiaServices.Tools
             List<string> arguments = new List<string>();
             SetArgumentListForConsoleApp(arguments, generatedFileName, options, sharedCodeServiceParameters, loggingServer.PipeName);
 
-            // TODO: Fix vulnerability with GetTempFileName, see https://sonarcloud.io/project/issues?resolved=false&severities=BLOCKER%2CCRITICAL%2CMAJOR%2CMINOR&sinceLeakPeriod=true&types=VULNERABILITY&pullRequest=414&id=OpenRIAServices_OpenRiaServices&open=AYi1D8MZVJzuBbc9Xd8Q&tab=why
-            // and add error handling 
-            string filename = Path.GetTempFileName();
-            File.WriteAllLines(filename, arguments);
-            startInfo.Arguments = "@" + filename;
+            string fileName = RiaClientFilesTaskHelpers.CreateAndWriteArgumentsToNewTempFile(arguments);
+
+            startInfo.Arguments = "@" + fileName;
 
             var process = Process.Start(startInfo);
 
@@ -828,7 +826,7 @@ namespace OpenRiaServices.Tools
                 }
                 else
                 {
-                    RiaClientFilesTaskHelpers.SafeFileDelete(filename, this);
+                    RiaClientFilesTaskHelpers.SafeFileDelete(fileName, this);
                 }
                 return success;
             }
