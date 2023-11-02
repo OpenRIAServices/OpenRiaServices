daa304e39d2cd3724a3246d08564111012485e6f
Daniel Svensson <daniel.svensson@hotmail.se>
2023-11-02 10:36:57 +0100

    Move more tests from EndToEnd tests to Client.Test (#454)
    * Move all EntityTests but 1 from EndToEnd to "Client.Tests"
    * move EntityConflictTests from E2E
    * move 2 more test classes

---
+++ src/OpenRiaServices.Client/Test/Client.Test/Data/DomainClients/City.partial.cs
@@ -0,0 +1,81 @@
+﻿extern alias SSmDsClient;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cities
+{
+    [CustomValidation(typeof(City), "ValidateCity")]
+    public partial class City
+    {
+        /// <summary>
+        /// Initializes a new instance of a <see cref="City"/> using the specified
+        /// state name, which can be invalid.
+        /// </summary>
+        /// <param name="stateName">
+        /// The state name to use for initialization. This can be
+        /// an invalid value, as validation won't be performed.
+        /// </param>
+        public City(string stateName)
+        {
+            this._stateName = stateName;
+        }
+
+        /// <summary>
+        /// Gets or sets whether or not the entity-level validation for ValidateCity should fail.
+        /// </summary>
+        internal bool ThrowValidationExceptions { get; set; }
+
+        /// <summary>
+        /// Gets or sets a callback to be used whenever the ValidateProperty method is invoked.
+        /// </summary>
+        internal Action<ValidationContext> ValidatePropertyCallback { get; set; }
+
+        /// <summary>
+        /// Gets or sets a callback to be used whenever the ValidateCity validation method is invoked.
+        /// </summary>
+        internal Action<ValidationContext> ValidateCityCallback { get; set; }
+
+        /// <summary>
+        /// Gets or sets the count of calls to the ValidateCity method, which is an
+        /// entity-level validation methods.
+        /// </summary>
+        internal int ValidateCityCallCount { get; set; }
+
+        protected override void ValidateProperty(ValidationContext context, object value)
+        {
+            this.ValidatePropertyCallback?.Invoke(context);
+
+            if (this.ThrowValidationExceptions)
+            {
+                System.ComponentModel.DataAnnotations.Validator.ValidateProperty(value, context);
+            }
+            else
+            {
+                base.ValidateProperty(context, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity-level custom validation should fail.
+        /// </summary>
+        public bool MakeEntityValidationFail { get; set; }
+
+        public static ValidationResult ValidateCity(City entity, ValidationContext validationContext)
+        {
+            entity.ValidateCityCallback?.Invoke(validationContext);
+
+            // Increment our call counter
+            ++entity.ValidateCityCallCount;
+
+            // And if we're supposed to fail, return the failure result
+            if (entity.MakeEntityValidationFail)
+            {
+                return new ValidationResult("MakeEntityValidationFail is true");
+            }
+
+            // Otherwise return success
+            return ValidationResult.Success;
+        }
+    }
+}
