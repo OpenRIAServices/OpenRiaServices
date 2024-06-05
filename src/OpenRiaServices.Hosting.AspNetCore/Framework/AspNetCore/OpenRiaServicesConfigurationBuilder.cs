f24b904208208ae0947e8c5a0a33d1a093b0d83b
Daniel Svensson <daniel.svensson@hotmail.se>
2024-06-05 15:17:44 +0200

    Remove public Map methods taking a explicit path

--- src/OpenRiaServices.Hosting.AspNetCore/Framework/AspNetCore/OpenRiaServicesConfigurationBuilder.cs
+++ src/OpenRiaServices.Hosting.AspNetCore/Framework/AspNetCore/OpenRiaServicesConfigurationBuilder.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using OpenRiaServices.Server;
 
@@ -35,28 +36,12 @@ namespace OpenRiaServices.Hosting.AspNetCore
             return _dataSource.AddDomainService(GetDomainServiceRoute(type), type);
         }
 
-        public IEndpointConventionBuilder AddDomainService(Type type, string path)
-        {
-            ArgumentNullException.ThrowIfNull(nameof(type));
-#if NET7_0_OR_GREATER
-            ArgumentNullException.ThrowIfNullOrEmpty(nameof(path));
-#endif
-
-            if (!_typeIsService.IsService(type))
-                throw new InvalidOperationException($"Domainservice {type} cannot be resolved by container, register it before calling map");
-
-            return _dataSource.AddDomainService(path, type);
-        }
-
-        // TODO: Remove
-        public IEndpointConventionBuilder AddDomainService<T>(string path) where T : DomainService
-        {
-            return AddDomainService(typeof(T), path);
-        }
-
         private static string GetDomainServiceRoute(Type type)
         {
-            // Lookup DomainServiceEndpointRoutePatternAttribute in same assembly as DomainService
+            // 1. TODO: Look at EnableClientAccessAttribute if we can set route there
+            // 2. Lookup DomainServiceEndpointRoutePatternAttribute in same assembly as DomainService
+            // 3. Lookup DomainServiceEndpointRoutePatternAttribute in startup assembly
+            // 4. Fallback to default (FullName)
             // - Fallback to FullName
             EndpointRoutePattern pattern =
                 type.Assembly.GetCustomAttribute<DomainServiceEndpointRoutePatternAttribute>()?.EndpointRoutePattern
