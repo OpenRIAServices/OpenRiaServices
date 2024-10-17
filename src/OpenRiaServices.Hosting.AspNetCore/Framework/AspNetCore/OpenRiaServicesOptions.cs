f2c086abe55a636f82d0aa4c5effa01508cd7252
Daniel Svensson <daniel.svensson@hotmail.se>
2024-10-17 19:48:09 +0200

    small code updates

--- src/OpenRiaServices.Hosting.AspNetCore/Framework/AspNetCore/OpenRiaServicesOptions.cs
+++ src/OpenRiaServices.Hosting.AspNetCore/Framework/AspNetCore/OpenRiaServicesOptions.cs
@@ -7,6 +7,8 @@ namespace OpenRiaServices.Hosting.AspNetCore
     public sealed class OpenRiaServicesOptions
     {
         //public Action<Exception, HttpContext> OnError { get; set; }
+        //EventHandler<DomainServiceErrorEventArgs> OnError;
+        // struct DomainServiceErrorEventArgs(Exception, Message? or IncludeExceptionMessageInFault, Stacktrace?, ErrorCode?, HttpResultCode? )
 
         /// <summary>
         /// Optional function to determine if the exception message should be included in the fault message.
@@ -19,7 +21,7 @@ namespace OpenRiaServices.Hosting.AspNetCore
         public bool? AlwaysIncludeExceptionMessageInFault { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the stack trace should be included in the error message.
+        /// Gets or sets a value indicating whether exception stack trace information should be included in error messages.
         /// <para>This is considered INSECURE since it gives an attacker to much information</para>
         /// </summary>
         public bool UnsafeIncludeStackTraceInErrors { get; set; }
--- src/OpenRiaServices.Hosting.AspNetCore/Framework/AspNetCore/OpenRiaServicesOptionsBuilder.cs
+++ src/OpenRiaServices.Hosting.AspNetCore/Framework/AspNetCore/OpenRiaServicesOptionsBuilder.cs
@@ -1,14 +1,14 @@
 ﻿#nullable enable
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using OpenRiaServices.Server;
 
 namespace OpenRiaServices.Hosting.AspNetCore
 {
+    /// <summary>
+    /// A possible future extension point for configuring OpenRia Services
+    /// Similar to <see cref="IMvcBuilder"/>.
+    /// </summary>
     public sealed class OpenRiaServicesOptionsBuilder
     {
         internal OpenRiaServicesOptionsBuilder(IServiceCollection services)
@@ -16,31 +16,6 @@ namespace OpenRiaServices.Hosting.AspNetCore
             ArgumentNullException.ThrowIfNull(services);
 
             Services = services;
-        }
-
-        /*** MOVE UP ***/
-        internal OpenRiaServicesOptionsBuilder AddDomainService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods | DynamicallyAccessedMemberTypes.PublicConstructors)] T>() where T : DomainService
-        {
-            return AddDomainService<T>(ServiceLifetime.Transient);
-        }
-
-        internal OpenRiaServicesOptionsBuilder AddDomainService<T>(ServiceLifetime serviceLifetime) where T : DomainService
-        {
-            Services.AddDomainService<T>(serviceLifetime);
-            return this;
-        }
-
-        internal void AddDomainServicesFromAssemblies(IEnumerable<Assembly> assemblies, ServiceLifetime serviceLifetime)
-        {
-            Services.AddDomainServicesFromAssemblies(assemblies, serviceLifetime);
-        }
-
-        /// <summary>
-        /// Registers public <see cref="DomainService"/>s that are marked with <see cref="EnableClientAccessAttribute"/> from <paramref name="assembly"/> 
-        /// </summary>
-        internal void AddDomainServicesFromAssembly(Assembly assembly, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
-        {
-            Services.AddDomainServicesFromAssembly(assembly, serviceLifetime);
         }
 
         internal IServiceCollection Services { get; }
