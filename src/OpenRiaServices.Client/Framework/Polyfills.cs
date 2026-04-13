b02b02cee7e3d97c72194bbf470600dd98bedded
Daniel Svensson <daniel.svensson@hotmail.se>
2026-04-13 19:09:02 +0200

    update polyfills

--- src/OpenRiaServices.Client/Framework/Polyfills.cs
+++ src/OpenRiaServices.Client/Framework/Polyfills.cs
@@ -128,12 +128,7 @@ namespace System.Diagnostics.CodeAnalysis
 
     /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-null values when returning with the specified return value condition.</summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
-#if SYSTEM_PRIVATE_CORELIB
-    public
-#else
-    internal
-#endif
-        sealed class MemberNotNullWhenAttribute : Attribute
+    internal sealed class MemberNotNullWhenAttribute : Attribute
     {
         /// <summary>Initializes the attribute with the specified return value condition and a field or property member.</summary>
         /// <param name="returnValue">
@@ -163,6 +158,26 @@ namespace System.Diagnostics.CodeAnalysis
 
         /// <summary>Gets the return value condition.</summary>
         public bool ReturnValue { get; }
+
+        /// <summary>Gets field or property member names.</summary>
+        public string[] Members { get; }
+    }
+
+    /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-null values.</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    internal sealed class MemberNotNullAttribute : Attribute
+    {
+        /// <summary>Initializes the attribute with a field or property member.</summary>
+        /// <param name="member">
+        /// The field or property member that is promised to be not-null.
+        /// </param>
+        public MemberNotNullAttribute(string member) => Members = [member];
+
+        /// <summary>Initializes the attribute with the list of field and property members.</summary>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.
+        /// </param>
+        public MemberNotNullAttribute(params string[] members) => Members = members;
 
         /// <summary>Gets field or property member names.</summary>
         public string[] Members { get; }
